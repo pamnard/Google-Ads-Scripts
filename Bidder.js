bd3e66b8f48d4080a88bf740a921e1ecd53ad475
pamnard <pamnard@users.noreply.github.com>
2017-09-15 13:42:05 +0300

    Update Bidder.js

--- Bidder.js
+++ Bidder.js
@@ -1,120 +1,95 @@
 function main() {
 
-    var ARPU = 13; // APRU
-    var minPosition = 1.5; // Минимально удерживаемая позиция
+    var ARPU = 15; // APRU на пользователя без партнёра
+    var AverageCheck = 190; // Средний чек на пользователя без партнёра
 
-    var adGroupPerfomanceAWQL = 'SELECT CampaignName, CampaignId, AdGroupName, AdGroupId ' +
-        'FROM ADGROUP_PERFORMANCE_REPORT ' +
-        'WHERE CampaignStatus = ENABLED AND AdGroupStatus = ENABLED AND CampaignName DOES_NOT_CONTAIN_IGNORE_CASE DSA AND CampaignName DOES_NOT_CONTAIN "[" ' +
+    ARPU = +Math.round(ARPU / 3);
+    AverageCheck = +Math.round(AverageCheck / 3);
+
+    var minPosition = 2; // Минимально удерживаемая позиция
+
+    var MILLIS_PER_DAY = 1000 * 60 * 60 * 24;
+    var now = new Date();
+    var nowDate = new Date(now.getTime());
+    var timeZone = AdWordsApp.currentAccount().getTimeZone();
+    var nowDateFormatted = Utilities.formatDate(nowDate, timeZone, 'HH');
+    if (nowDateFormatted < 2) {
+        nowDateFormatted = 2;
+    }
+
+    var campaignPerfomaceAWQL = 'SELECT CampaignName, CampaignId ' +
+        'FROM CAMPAIGN_PERFORMANCE_REPORT ' +
+        'WHERE CampaignStatus = ENABLED AND AdvertisingChannelType = SEARCH ' + // AND CampaignName DOES_NOT_CONTAIN_IGNORE_CASE DSA AND CampaignName DOES_NOT_CONTAIN "["
         'DURING TODAY';
-
-    var adGroupPerfomanceRowsIter = AdWordsApp.report(adGroupPerfomanceAWQL).rows();
-
-    while (adGroupPerfomanceRowsIter.hasNext()) {
-        var keywordRow = adGroupPerfomanceRowsIter.next();
-        var keywordCampaignName = keywordRow['CampaignName'];
-        var keywordCampaignId = keywordRow['CampaignId'];
-        var keywordAdGroupName = keywordRow['AdGroupName'];
-        var keywordAdGroupId = keywordRow['AdGroupId'];
-
-        if (keywordRow != undefined) {
-            Logger.log('Campaign: ' + keywordCampaignName + ', Ad Group: ' + keywordAdGroupName);
-            lowPosition();
-            firstPage();
-            Logger.log('-----------------------------------------------------------------------------------------');
+    var campaignPerfomaceRowsIter = AdWordsApp.report(campaignPerfomaceAWQL).rows();
+    while (campaignPerfomaceRowsIter.hasNext()) {
+        var CampaignRow = campaignPerfomaceRowsIter.next();
+        var CampaignName = CampaignRow['CampaignName'];
+        var CampaignId = CampaignRow['CampaignId'];
+        if (CampaignRow) {
+            getAdGroups();
         }
     }
 
-    function lowPosition() {
+    function getAdGroups() {
+        var adGroupPerfomanceAWQL = 'SELECT AdGroupName, AdGroupId ' +
+            'FROM ADGROUP_PERFORMANCE_REPORT ' +
+            'WHERE CampaignId = ' + CampaignId + ' AND AdGroupStatus = ENABLED  ' +
+            'DURING TODAY';
 
-        var MILLIS_PER_DAY = 1000 * 60 * 60 * 24;
-        var now = new Date();
-        var nowDate = new Date(now.getTime());
-        var timeZone = AdWordsApp.currentAccount().getTimeZone();
-        var nowDateFormatted = Utilities.formatDate(nowDate, timeZone, 'HH');
-        if (nowDateFormatted < 2) {
-            nowDateFormatted = 2;
+        var adGroupPerfomanceRowsIter = AdWordsApp.report(adGroupPerfomanceAWQL).rows();
+
+        while (adGroupPerfomanceRowsIter.hasNext()) {
+            var AdGroupRow = adGroupPerfomanceRowsIter.next();
+            var AdGroupName = AdGroupRow['AdGroupName'];
+            var AdGroupId = AdGroupRow['AdGroupId'];
+
+            if (AdGroupRow != undefined) {
+                Logger.log('Campaign: ' + CampaignName + ', Ad Group: ' + AdGroupName);
+                lowPosition();
+                firstPage();
+                Logger.log('-----------------------------------------------------------------------------------------');
+            }
         }
 
-        var awql = 'SELECT CampaignId, AdGroupId, Impressions, IsNegative, AveragePosition, CpcBid ' +
-            'FROM KEYWORDS_PERFORMANCE_REPORT ' +
-            'WHERE CampaignId = ' + keywordCampaignId + ' AND AdGroupId = ' + keywordAdGroupId + ' AND Status = ENABLED AND CpcBid > 0 AND AveragePosition > ' + minPosition + ' AND Impressions > ' + nowDateFormatted + ' ' +
-            'DURING TODAY';
-        // Logger.log(awql);
-
-        var rowsIter = AdWordsApp.report(awql).rows();
-
-        while (rowsIter.hasNext()) {
-            var row = rowsIter.next();
-            var Impressions = row['Impressions'];
-            var IsNegative = row['IsNegative'];
-            var AveragePosition = row['AveragePosition'];
-            var CpcBid = row['CpcBid'].toString();
-            var CpcBidInMicros = (CpcBid * 1000000);
-            CpcBidInMicros = CpcBidInMicros.toFixed();
-
-            if (IsNegative == 'false') {
-                var keywordIterator = AdWordsApp.keywords()
-                    .withCondition('CampaignId = ' + keywordCampaignId)
-                    .withCondition('AdGroupId = ' + keywordAdGroupId)
-                    .withCondition('AveragePosition > ' + minPosition)
-                    .withCondition('Impressions > ' + nowDateFormatted)
-                    .forDateRange('TODAY')
-                    .get();
-                if (keywordIterator.hasNext()) {
-                    while (keywordIterator.hasNext()) {
-                        var keyword = keywordIterator.next();
-                        var keywordBidding = keyword.bidding();
-                        var keywordCpc = keywordBidding.getCpc();
-                        keywordCpc = keywordCpc.toString();
-                        keyword.bidding().setCpc(bidCpc(keywordCpc));
-                        Logger.log('Повышаем позицию');
-                        Logger.log('Keyword: ' + keyword.getText() + ' Impressions: ' + Impressions + ' AveragePosition: ' + AveragePosition + ' OldCPC: ' + keywordCpc + ' NewCPC: ' + bidCpc(keywordCpc));
-                    }
+        function lowPosition() {
+            var keywordIterator = AdWordsApp.keywords()
+                .withCondition('CampaignId = ' + CampaignId)
+                .withCondition('AdGroupId = ' + AdGroupId)
+                .withCondition('AveragePosition > ' + minPosition)
+                .withCondition('Impressions > ' + nowDateFormatted)
+                .withCondition('Status = ENABLED')
+                .forDateRange('TODAY')
+                .get();
+            if (keywordIterator.hasNext()) {
+                while (keywordIterator.hasNext()) {
+                    var keyword = keywordIterator.next();
+                    var keywordBidding = keyword.bidding();
+                    var keywordCpc = keywordBidding.getCpc();
+                    keywordCpc = keywordCpc.toString();
+                    keyword.bidding().setCpc(bidCpc(keywordCpc));
+                    Logger.log('Повышаем позицию');
+                    Logger.log('Keyword: ' + keyword.getText() + ' OldCPC: ' + keywordCpc + ' NewCPC: ' + bidCpc(keywordCpc));
                 }
             }
         }
 
-    }
-
-    function firstPage() {
-
-        var awql = 'SELECT Id, IsNegative, FirstPageCpc, CpcBid ' +
-            'FROM KEYWORDS_PERFORMANCE_REPORT ' +
-            'WHERE CampaignId = ' + keywordCampaignId + ' AND AdGroupId = ' + keywordAdGroupId + ' AND Status = ENABLED AND CpcBid > 0 AND FirstPageCpc > 0 ' +
-            'DURING TODAY';
-        // Logger.log(awql);
-
-        var rowsIter = AdWordsApp.report(awql).rows();
-
-        while (rowsIter.hasNext()) {
-            var row = rowsIter.next();
-            var Id = row['Id'];
-            var IsNegative = row['IsNegative'];
-            var FirstPageCpc = row['FirstPageCpc'].toString();
-            var FirstPageCpcInMicros = (FirstPageCpc * 1000000);
-            FirstPageCpcInMicros = FirstPageCpcInMicros.toFixed();
-            var CpcBid = row['CpcBid'].toString();
-            var CpcBidInMicros = (CpcBid * 1000000);
-            CpcBidInMicros = CpcBidInMicros.toFixed();
-
-            if ((IsNegative == 'false') && (CpcBidInMicros < FirstPageCpcInMicros)) {
-                var keywordIterator = AdWordsApp.keywords()
-                    .withCondition('CampaignId = ' + keywordCampaignId)
-                    .withCondition('AdGroupId = ' + keywordAdGroupId)
-                    .withCondition('Id = ' + Id)
-                    .withCondition('CpcBid < ' + FirstPageCpcInMicros)
-                    .get();
-                if (keywordIterator.hasNext()) {
-                    while (keywordIterator.hasNext()) {
-                        var keyword = keywordIterator.next();
-                        var keywordBidding = keyword.bidding();
-                        var keywordCpc = keywordBidding.getCpc();
-                        keywordCpc = keywordCpc.toString();
+        function firstPage() {
+            var keywordIterator = AdWordsApp.keywords()
+                .withCondition('CampaignId = ' + CampaignId)
+                .withCondition('AdGroupId = ' + AdGroupId)
+                .withCondition('Status = ENABLED')
+                .get();
+            if (keywordIterator.hasNext()) {
+                while (keywordIterator.hasNext()) {
+                    var keyword = keywordIterator.next();
+                    var keywordFirstPageCpc = parseFloat(keyword.getFirstPageCpc()).toFixed(2);
+                    var keywordCpc = parseFloat(keyword.bidding().getCpc()).toFixed(2);
+                    if (keywordFirstPageCpc > keywordCpc) {
                         keyword.bidding().setCpc(bidCpc(keywordCpc));
-                        Logger.log('Выводим на 1-ю страницу');
-                        Logger.log('Keyword: ' + keyword.getText() + ' OldCPC: ' + keywordCpc + ' NewCPC: ' + bidCpc(keywordCpc));
                     }
+                    Logger.log('Выводим на 1-ю страницу');
+                    Logger.log('Keyword: ' + keyword.getText() + ' OldCPC: ' + keywordCpc + ' NewCPC: ' + bidCpc(keywordCpc));
                 }
             }
         }

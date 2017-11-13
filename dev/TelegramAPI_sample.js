de94a9f9f0ddbbc50fe6beca569aebb300d21240
Pamnard <pamnard@users.noreply.github.com>
2017-11-14 00:25:32 +0300

    Update TelegramAPI_sample.js

--- dev/TelegramAPI_sample.js
+++ dev/TelegramAPI_sample.js
@@ -4,7 +4,7 @@ function main() {
 		// Токен надо получить у BotFather, создав нового бота
 		TOKEN: '0987654321:QQQWWWEEERRRTTTYYYUUUIIIOOOPPP111222333',
 
-		// Напишите что-нибудь в чат вашему боту, посе чего перейдите по ссылке https://api.telegram.org/bot<ТОКЕН>/getUpdates
+		// Напишите что-нибудь в чат вашему боту, после чего перейдите по ссылке https://api.telegram.org/bot<ТОКЕН>/getUpdates
 		// в ответном тексте найдите строку ..."chat":{"id":123456789,"first_name"... Нужно значение id.
 		CHAT_ID: '123456789'
 	};

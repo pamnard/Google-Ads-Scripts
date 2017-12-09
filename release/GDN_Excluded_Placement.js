cc4bb95f9051c7296ded9fb5a810de8f640d003d
Pamnard <pamnard@users.noreply.github.com>
2017-12-09 20:30:28 +0300

    Update GDN_Excluded_Placement.js

--- release/GDN_Excluded_Placement.js
+++ release/GDN_Excluded_Placement.js
@@ -1,29 +1,34 @@
 function main() {
 
-    /**
-     * Скрипт добавляет площадки в список исключений по заданным параметрам
-     **/
+    // Скрипт добавляет площадки в список исключений по заданным параметрам
+
+    var CONFIG = {
+        ARPU: 15,
+        // Средняя выручка на конверсию
+
+        AverageCheck: 190,
+        // Средняя выручка на конверсию с Value
+
+        customDaysInDateRange: 180,
+        // Указываем количество дней для выборки
+        // Если хотим использовать данные о конверсиях или доходности, то в качестве значения следует указывать число больее чем окно конверсии. 
+
+        customDateRangeShift: 0
+        // Указываем на сколько дней от сегодняшнего мы сдвигаем выборку. Нужно для того чтобы не брать те дни когда запаздывает статистика.
+        // Если хотим использовать данные о конверсиях или доходности, то в качестве значения следует указывать число равное дням в окне конверсии. 
+    };
+
+    //======================================================================
 
     var REPORTING_OPTIONS = {
         // Comment out the following line to default to the latest reporting version.
         apiVersion: 'v201705'
     };
 
-    var ARPU = 15;
-    var AverageCheck = 190;
-
-    // Указываем количество дней для выборки
-    // Если хотим использовать данные о конверсиях или доходности, то в качестве значения следует указывать число больее чем окно конверсии. 
-    var customDaysInDateRange = 180;
-
-    // Указываем на сколько дней от сегодняшнего мы сдвигаем выборку. Нужно для того чтобы не брать те дни когда запаздывает статистика.
-    // Если хотим использовать данные о конверсиях или доходности, то в качестве значения следует указывать число равное дням в окне конверсии. 
-    var customDateRangeShift = 0;
-
     var campaignPerfomaceAWQL = 'SELECT CampaignName, CampaignId ' +
         'FROM CAMPAIGN_PERFORMANCE_REPORT ' +
         'WHERE CampaignStatus = ENABLED AND AdvertisingChannelType = DISPLAY ' +
-        'AND Cost > ' + (ARPU * 1000000) + ' ' + 
+        'AND Cost > ' + (CONFIG.ARPU * 1000000) + ' ' +
         'DURING ' + customDateRange();
     var campaignPerfomaceRowsIter = AdWordsApp.report(campaignPerfomaceAWQL, REPORTING_OPTIONS).rows();
     while (campaignPerfomaceRowsIter.hasNext()) {
@@ -39,7 +44,7 @@ function main() {
         var AdGroupPerfomanceAWQL = 'SELECT AdGroupName, AdGroupId ' +
             'FROM ADGROUP_PERFORMANCE_REPORT ' +
             'WHERE CampaignId = ' + CampaignId + ' AND AdGroupStatus = ENABLED ' +
-            'AND Cost > ' + (ARPU * 1000000) + ' ' + 
+            'AND Cost > ' + (CONFIG.ARPU * 1000000) + ' ' +
             'DURING ' + customDateRange();
         var AdGroupPerfomancerowsIter = AdWordsApp.report(AdGroupPerfomanceAWQL, REPORTING_OPTIONS).rows();
         while (AdGroupPerfomancerowsIter.hasNext()) {
@@ -53,25 +58,24 @@ function main() {
         }
 
         function placemetsBlock() {
-            var awql = 'SELECT Criteria, Clicks, Conversions, Cost, ConversionValue, CostPerConversion, IsNegative ' +
+            var AWQL = 'SELECT Criteria, Clicks, Conversions, Cost, ConversionValue, CostPerConversion, IsNegative ' +
                 'FROM PLACEMENT_PERFORMANCE_REPORT ' +
                 'WHERE CampaignId = ' + CampaignId + ' AND AdGroupId = ' + AdGroupId + ' AND IsNegative = FALSE ' +
-                'AND Cost > ' + (ARPU * 1000000) + ' ' +
+                'AND Cost > ' + (CONFIG.ARPU * 1000000) + ' ' +
                 'DURING ' + customDateRange(); //
-            var rowsIter = AdWordsApp.report(awql).rows();
+            var rowsIter = AdWordsApp.report(AWQL).rows();
             while (rowsIter.hasNext()) { // определяем исключаемые площадки
-                var row = rowsIter.next();
-                var Domain = row['Criteria'].toString();
-                var Clicks = parseFloat(row['Clicks']).toFixed();
-                var Conversions = parseFloat(row['Conversions']).toFixed(2);
-                var ConversionValue = parseFloat(row['ConversionValue']).toFixed();
-                var CostPerConversion = parseFloat(row['CostPerConversion']).toFixed(2);
-                var Cost = parseFloat(row['Cost']).toFixed(2);
-                var IsNegative = row['IsNegative'];
-                var CustomConversionRate = (Conversions / Clicks) * 100;
-                CustomConversionRate = +CustomConversionRate.toFixed(2);
-                if (ConversionValue == 0) {
-                    if ((Conversions < 0.01) && (Cost > (ARPU * 3))) { // на 2 ARPU нет конверсий
+                var row = rowsIter.next(),
+                    Domain = row['Criteria'].toString(),
+                    Clicks = parseFloat(row['Clicks']).toFixed(),
+                    Conversions = parseFloat(row['Conversions']).toFixed(2),
+                    ConversionValue = parseFloat(row['ConversionValue']).toFixed(),
+                    CostPerConversion = parseFloat(row['CostPerConversion']).toFixed(2),
+                    Cost = parseFloat(row['Cost']).toFixed(2),
+                    IsNegative = row['IsNegative'].toString(),
+                    CustomConversionRate = ((Conversions / Clicks) * 100).toFixed(2);
+                if (ConversionValue == +0) {
+                    if ((Conversions < 0.01) && (Cost > (CONFIG.ARPU * 3))) { // на 2 ARPU нет конверсий
                         addNegativeKeywordToAdGroup(Domain);
                         Logger.log('Исключаем ' + Domain + ' - нет конверсий при большом расходе (больше 2-х ARPU).');
                         Logger.log('Клики - ' + Clicks +
@@ -82,7 +86,7 @@ function main() {
                             ' Доход с площадки - ' + ConversionValue);
                         Logger.log('------------------------------------');
                     }
-                    if ((Conversions > 0) && (Cost > (ARPU * 3)) && (Conversions < 3) && (CostPerConversion > (ARPU * 2))) { // 1-2 сильно дорогие конверсии
+                    if ((Conversions > +0) && (Cost > (CONFIG.ARPU * 3)) && (Conversions < 3) && (CostPerConversion > (CONFIG.ARPU * 2))) { // 1-2 сильно дорогие конверсии
                         addNegativeKeywordToAdGroup(Domain);
                         Logger.log('Исключаем ' + Domain + ' - конверсий мало, а те что есть очень дорогие.');
                         Logger.log('Клики - ' + Clicks +
@@ -94,7 +98,7 @@ function main() {
                         Logger.log('------------------------------------');
                     }
 
-                    if ((Conversions > 2) && (Cost > (ARPU * 2)) && (CostPerConversion > ARPU)) { // конверсий много, но они дороже ARPU
+                    if ((Conversions > 2) && (Cost > (CONFIG.ARPU * 2)) && (CostPerConversion > CONFIG.ARPU)) { // конверсий много, но они дороже ARPU
                         addNegativeKeywordToAdGroup(Domain);
                         Logger.log('Исключаем ' + Domain + ' - конверсий много, но они дороже ARPU.');
                         Logger.log('Клики - ' + Clicks +
@@ -105,7 +109,7 @@ function main() {
                             ' Доход с площадки - ' + ConversionValue);
                         Logger.log('------------------------------------');
                     }
-                    if ((Cost > AverageCheck)) { // ROI = 0 
+                    if ((Cost > CONFIG.AverageCheck)) { // ROI = 0 
                         addNegativeKeywordToAdGroup(Domain);
                         Logger.log('Исключаем ' + Domain + ' - ROI = 0.');
                         Logger.log('Клики - ' + Clicks +
@@ -146,26 +150,17 @@ function main() {
     }
 
     function customDateRange(select) { // Формируем значение параметра временного диапазона для выборки AWQL
-        var timeType = select;
-        var MILLIS_PER_DAY = 1000 * 60 * 60 * 24;
-        var now = new Date();
-        var fromDate = new Date(now.getTime() - (customDaysInDateRange + customDateRangeShift) * MILLIS_PER_DAY);
-        var toDate = new Date(now.getTime() - customDateRangeShift * MILLIS_PER_DAY);
-        var nowDate = new Date(now.getTime());
-        var timeZone = AdWordsApp.currentAccount().getTimeZone();
-        var fromformatDate = Utilities.formatDate(fromDate, timeZone, 'yyyyMMdd');
-        var toformatDate = Utilities.formatDate(toDate, timeZone, 'yyyyMMdd');
-        var nowformatDate = Utilities.formatDate(nowDate, timeZone, 'yyyyMMdd');
-        var duringDates = fromformatDate + ',' + toformatDate;
-
-        if (timeType == 'from') {
-            return fromformatDate;
-        } else if (timeType == 'to') {
-            return toformatDate;
-        } else if (timeType == 'now') {
-            return nowformatDate;
-        } else {
-            return duringDates;
-        }
+        var timeType = select,
+            MILLIS_PER_DAY = 1000 * 60 * 60 * 24,
+            now = new Date(),
+            fromDate = new Date(now.getTime() - (customDaysInDateRange + customDateRangeShift) * MILLIS_PER_DAY),
+            toDate = new Date(now.getTime() - customDateRangeShift * MILLIS_PER_DAY),
+            nowDate = new Date(now.getTime()),
+            timeZone = AdWordsApp.currentAccount().getTimeZone(),
+            fromformatDate = Utilities.formatDate(fromDate, timeZone, 'yyyyMMdd'),
+            toformatDate = Utilities.formatDate(toDate, timeZone, 'yyyyMMdd'),
+            nowformatDate = Utilities.formatDate(nowDate, timeZone, 'yyyyMMdd'),
+            duringDates = fromformatDate + ',' + toformatDate;
+        return duringDates;
     }
 }
